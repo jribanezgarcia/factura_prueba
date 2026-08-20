97acdc55c5742e55e5627560f050dbd210ad2b85
Juan Rafael Ibáñez García <jribanezgarcia@gmail.com>
2026-08-20 12:14:07 +0200

    Ignorar target, .idea y artefactos locales

---
+++ src/main/java/com/alcazaba/facturacion/Launcher.java
@@ -0,0 +1,9 @@
+package examen;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(MainApp.class, args);
+    }
+}
